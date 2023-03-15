75c354a866dbe363372f2a13878679e5baf35f30
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-15 18:55:14 -0500

    Cálculo cuadrado y cubo en Python

---
+++ parte001/ex005_cuadrado_cubo.py
@@ -0,0 +1,16 @@
+# Elabore un algoritmo que lea un número y obtenga su cuadrado y su cubo.
+
+# Solicitar el valor de A:
+A = int(input("Ingrese el valor de A: "))
+
+# Realizar las operaciones:
+
+# Cuadrado:
+cuadrado = A ** 2
+
+# Cubo:
+cubo = A ** 3
+
+# Mostrar los resultados:
+print("El cuadrado de A es: ", cuadrado)
+print("El cubo de A es: ", cubo)
