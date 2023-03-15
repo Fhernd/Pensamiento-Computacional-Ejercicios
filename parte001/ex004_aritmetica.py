d709a64c0aca91802880603923fd6fe835b89a35
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-15 18:21:24 -0500

    Aritmética básica en Python

---
+++ parte001/ex004_aritmetica.py
@@ -0,0 +1,34 @@
+#  Escriba un algoritmo que, dados dos valores A y B, encuentre:  
+# A - B 
+# A + B 
+# A * B 
+# A / B
+# A % B
+
+# Solicitar los valores A y B:
+A = int(input("Ingrese el valor de A: "))
+B = int(input("Ingrese el valor de B: "))
+
+# Realizar las operaciones:
+
+# Resta:
+resta = A - B
+
+# Suma:
+suma = A + B
+
+# Multiplicación:
+multiplicacion = A * B
+
+# División:
+division = A / B
+
+# Módulo:
+modulo = A % B
+
+# Mostrar los resultados:
+print("El resultado de la resta es: ", resta)
+print("El resultado de la suma es: ", suma)
+print("El resultado de la multiplicación es: ", multiplicacion)
+print("El resultado de la división es: ", division)
+print("El resultado del módulo es: ", modulo)
