df3cd30afdca0842dba590fb1d0d1c8849e55d10
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-15 18:31:32 -0500

    Aritmética básica en JavaScript

---
+++ parte001/ex004_aritmetica.js
@@ -0,0 +1,21 @@
+//  Escriba un algoritmo que, dados dos valores A y B, encuentre:  
+// A - B 
+// A + B 
+// A * B 
+// A / B
+// A % B
+
+const a = 10;
+const b = 5;
+
+const resta = a - b;
+const suma = a + b;
+const producto = a * b;
+const cociente = a / b;
+const resto = a % b;
+
+console.log("La resta es: " + resta);
+console.log("La suma es: " + suma);
+console.log("El producto es: " + producto);
+console.log("El cociente es: " + cociente);
+console.log("El resto es: " + resto);
