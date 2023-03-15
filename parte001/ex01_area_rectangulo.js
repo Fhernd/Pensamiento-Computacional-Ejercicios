96aa90fe5e6093a1f3e8c79a17c19ecc9634d438
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-14 20:42:44 -0500

    Área y perímetro en JavaScript

---
+++ parte001/ex01_area_rectangulo.js
@@ -0,0 +1,10 @@
+// Calcular el área y el perímetro de un rectángulo:
+
+var base = 10;
+var altura = 5;
+
+var area = base * altura;
+var perimetro = 2 * (base + altura);
+
+console.log("El área del rectángulo es: " + area);
+console.log("El perímetro del rectángulo es: " + perimetro);
