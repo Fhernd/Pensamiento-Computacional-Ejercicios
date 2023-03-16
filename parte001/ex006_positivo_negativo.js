38778de009a46908b7da026adb1593ae76c36668
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-15 21:15:36 -0500

    Números negativos y positivos en JavaScript

---
+++ parte001/ex006_positivo_negativo.js
@@ -0,0 +1,7 @@
+//  Elabore un algoritmo que lea un número negativo e imprima el número y el positivo del mismo. 
+
+const numero = -5;
+
+const positivo = -numero;
+
+console.log("El número " + numero + " es positivo: " + positivo);
