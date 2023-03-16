04f42efdabac24fbf819caae5c52552a10d9bf1f
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-15 21:12:23 -0500

    Números negativos y positivos en Python

---
+++ parte001/ex006_positivo_negativo.py
@@ -0,0 +1,14 @@
+#  Elabore un algoritmo que lea un número negativo e imprima el número y el positivo del mismo. 
+
+# Declaración de variables
+numero = 0
+
+# Lectura de datos
+numero = int(input("Ingrese un número negativo: "))
+
+# Proceso
+
+# Salida de datos
+
+print("El número ingresado es: ", numero)
+print("El número positivo es: ", numero * -1)
