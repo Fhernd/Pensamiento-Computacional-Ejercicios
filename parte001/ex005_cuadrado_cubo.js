4fff917d206d6d82af8dfbaee69c94081a04ac33
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-15 19:03:12 -0500

    Cálculo cuadrado y cubo en JavaScript

---
+++ parte001/ex005_cuadrado_cubo.js
@@ -0,0 +1,9 @@
+// Elabore un algoritmo que lea un número y obtenga su cuadrado y su cubo.
+
+const numero = 5;
+
+const cuadrado = numero * numero;
+const cubo = cuadrado * numero;
+
+console.log("El cuadrado de " + numero + " es: " + cuadrado);
+console.log("El cubo de " + numero + " es: " + cubo);
