cf769a1e7a62f5df2a770b10fcdfe9d65a7c9052
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-19 05:30:09 -0500

    Cálculo del área de un círculo y longitud de la circunferencia en Python

---
+++ parte001/ex008_area_longitud_circulo.py
@@ -0,0 +1,10 @@
+# Dado el radio de un círculo. Haga un algoritmo que obtenga el área del círculo y la longitud 
+# de la circunferencia.
+
+radio = float(input("Ingrese el radio del círculo: "))
+
+area = 3.1416 * radio ** 2
+longitud = 2 * 3.1416 * radio
+
+print("El área del círculo es: ", area)
+print("La longitud de la circunferencia es: ", longitud)
