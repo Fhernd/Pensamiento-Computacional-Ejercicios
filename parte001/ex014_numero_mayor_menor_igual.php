54ca3e28948a3cfdb2b48657b9bdef4e289dda39
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-20 11:38:47 -0500

    Determinar si A es mayor, menor o igual a B en PHP

---
+++ parte001/ex014_numero_mayor_menor_igual.php
@@ -0,0 +1,14 @@
+<?php
+// Hacer un algoritmo que, dados dos valores numéricos A y B, escriba un mensaje diciendo 
+// si A es mayor, menor o igual a B.
+
+$a = 5;
+$b = 10;
+
+if ($a > $b) {
+    echo "$a es mayor que $b";
+} else if ($a < $b) {
+    echo "$a es menor que $b";
+} else {
+    echo "$a es igual que $b";
+}
