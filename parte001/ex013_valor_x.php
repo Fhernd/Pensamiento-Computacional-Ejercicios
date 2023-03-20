ca61ce1bbdbc756fba0bf466fad31690cd3ff13e
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-20 11:33:27 -0500

    Valor de X en PHP

---
+++ parte001/ex013_valor_x.php
@@ -0,0 +1,15 @@
+<?php
+// Escribir las estructuras que calculen y muestren el valor de X de acuerdo a lo siguiente: 
+// X = 1 si  Y > Z 
+// X = 2 si  Y <= Z
+
+$y = 5;
+$z = 10;
+
+if ($y > $z) {
+    $x = 1;
+} else {
+    $x = 2;
+}
+
+echo "x = $x";
