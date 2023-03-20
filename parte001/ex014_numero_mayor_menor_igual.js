1d7115c413dc45d74dfbbed5fb84da0512b0bac3
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-20 11:38:18 -0500

    Determinar si A es mayor, menor o igual a B en JavaScript

---
+++ parte001/ex014_numero_mayor_menor_igual.js
@@ -0,0 +1,13 @@
+// Hacer un algoritmo que, dados dos valores numéricos A y B, escriba un mensaje diciendo 
+// si A es mayor, menor o igual a B.
+
+const a = 5;
+const b = 10;
+
+if (a > b) {
+    console.log("A es mayor que B");
+} else if (a < b) {
+    console.log("A es menor que B");
+} else {
+    console.log("A es igual a B");
+}
