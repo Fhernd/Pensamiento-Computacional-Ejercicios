3fde14654ae7b9d9c29547bbfef2021e3a81e95f
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-03-20 11:32:52 -0500

    Valor de X en JavaScript

---
+++ parte001/ex013_valor_x.js
@@ -0,0 +1,16 @@
+// Escribir las estructuras que calculen y muestren el valor de X de acuerdo a lo siguiente: 
+// X = 1 si  Y > Z 
+// X = 2 si  Y <= Z
+
+const y = 5;
+const z = 10;
+
+let x = 0;
+
+if (y > z) {
+    x = 1;
+} else {
+    x = 2;
+}
+
+console.log("x = " + x);
